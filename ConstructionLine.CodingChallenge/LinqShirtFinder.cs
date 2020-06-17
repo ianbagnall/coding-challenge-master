a44838d4173e3a07a5a6858050f233c33d3522f1
Ian Bagnall <ianbagnall5@hotmail.com>
2020-06-17 08:51:32 +0100

    Method Renames

--- ConstructionLine.CodingChallenge/LinqShirtFinder.cs
+++ ConstructionLine.CodingChallenge/LinqShirtFinder.cs
@@ -23,18 +23,19 @@ namespace ConstructionLine.CodingChallenge
             var searchResults = new SearchResults
             {
                 Shirts = selectedShirts,
-                SizeCounts = GetCountsOfEachSize(selectedShirts),
-                ColorCounts = GetCountsOfEachColor(selectedShirts)
+                SizeCounts = GroupCountOfEachSize(selectedShirts),
+                ColorCounts = GroupCountOfEachColor(selectedShirts)
             };
 
             return searchResults;
         }
 
         private List<Shirt> GetShirtsByColorAndSize(SearchOptions options) =>
+
              _shirts.Where(shirt => (!options.Colors.Any() || options.Colors.Any(x => x == shirt.Color)) &&
                                     (!options.Sizes.Any() || options.Sizes.Any(x => x == shirt.Size))).ToList();
 
-        private List<ColorCount> GetCountsOfEachColor(List<Shirt> selectedShirts)
+        private List<ColorCount> GroupCountOfEachColor(List<Shirt> selectedShirts)
         {
             var colorCounts = selectedShirts.GroupBy(shirt => shirt.Color)
                 .Select(group => new ColorCount
@@ -43,16 +44,16 @@ namespace ConstructionLine.CodingChallenge
                     Count = group.Count()
                 }).ToList();
 
-            GetMissingColors(colorCounts);
+            GroupMissingColors(colorCounts);
 
             return colorCounts;
         }
 
-        private static void GetMissingColors(List<ColorCount> colorCounts) => Color.All
+        private static void GroupMissingColors(List<ColorCount> colorCounts) => Color.All
                 .Where(color => !colorCounts.Any(x => x.Color == color)).ToList()
                 .ForEach(color => colorCounts.Add(new ColorCount {Color = color, Count = 0}));
 
-        private List<SizeCount> GetCountsOfEachSize(List<Shirt> selectedShirts)
+        private List<SizeCount> GroupCountOfEachSize(List<Shirt> selectedShirts)
         {
             var sizeCounts = selectedShirts.GroupBy(shirt => shirt.Size)
                 .Select(group => new SizeCount
@@ -61,12 +62,12 @@ namespace ConstructionLine.CodingChallenge
                     Count = group.Count()
                 }).ToList();
 
-            GetMissingSizes(sizeCounts);
+            GroupMissingSizes(sizeCounts);
 
             return sizeCounts;
         }
 
-        private static void GetMissingSizes(List<SizeCount> sizeCounts) => Size.All
+        private static void GroupMissingSizes(List<SizeCount> sizeCounts) => Size.All
             .Where(size => !sizeCounts.Any(x => x.Size == size)).ToList()
             .ForEach(size => sizeCounts.Add(new SizeCount {Size = size, Count = 0}));
     }
